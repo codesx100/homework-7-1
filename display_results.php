0398e0c88a81663322a90809046e5089753c83fa
Oliver Howarth <oah5@njit.edu>
2017-07-11 17:28:02 -0400

    added homework code

--- display_results.php
+++ display_results.php
@@ -1,14 +1,33 @@
 <?php
     // get the data from the form
     $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $password = filter_input(INPUT_POST, 'password');
+    $phone = filter_input(INPUT_POST,'phone');
 
     // get the rest of the data for the form
 
     // for the heard_from radio buttons,
     // display a value of 'Unknown' if the user doesn't select a radio button
+    
+    $heard_from = filter_input(INPUT_POST, 'heard_from');
+    if ($heard_from === NULL) {
+        $heard_from = 'Unknown';
+    }
 
     // for the wants_updates check box,
     // display a value of 'Yes' or 'No'
+    if (isset($wants_updates)) {
+        $wants_updates = 'Yes';
+    } else {
+        $wants_updates = 'No';
+    }
+
+    $contact_via = filter_input(INPUT_POST, 'contact_via');
+
+    $comments = filter_input(INPUT_POST, 'comments');
+    $comments = htmlspecialchars($comments);
+    $comments = nl2br($comments, false);    
+    
 ?>
 <!DOCTYPE html>
 <html>
